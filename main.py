4841c36ee305a7a52735466493718f79b5b332e2
Sebas-blt <sebasat@icloud.com>
2026-03-25 12:07:47 +0100

    Modified main and input files
    Modified input functions to save the date searched at entsoe to csv files, to work faster and make them available for everyone.

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ CONFIG_DICT = {
     "neighbor_countries": ["FR", "NL", "DE_LU"],    # must be the same naming convention as used by ENTSO-E
     "default_year": 2023,   # data in good quality available from ENTSO-E for 2015-2024
     "scenario_years": 2021, # data in good quality available from ENTSO-E for 2015-2024
-    "technologies": ["solar", "solar-rooftop", "solar-rooftop commercial"]  # must be the same naming convention as in cost list
+    "technologies": ["solar", "onwind", "offwind","CCGT","nuclear"]  # must be the same naming convention as in cost list
 }
 
 VOLTAGE_LEVEL = 400 # kV, specified by assignment guidelines
 REACTANCE = 0.1 # specified by assignment guidelines
 
 inp_hndl = InputHandler(CONFIG_DICT)
-print(inp_hndl.load_BE_2022.shape)
+print(inp_hndl.load_BE_2023.shape)
--- src/input.py
+++ src/input.py
@@ -1,32 +1,39 @@
 from entsoe import EntsoePandasClient
 import pandas as pd 
+from pathlib import Path
 
 class InputHandler():
     def __init__(self, config_dict:dict):
+        # Create cache directory if it doesn't exist
+        self.cache_dir = Path("entsoe_data")
+        self.cache_dir.mkdir(exist_ok=True)
+
         self.client = EntsoePandasClient(api_key='5535fa5d-0280-43f2-9257-0a9295e5105e')
         start = pd.Timestamp(f'{config_dict["default_year"]}-01-01', tz="Europe/Brussels")
         end = pd.Timestamp(f'{config_dict["default_year"] + 1}-01-01', tz="Europe/Brussels")
 
         # for default year, get load and cf of main country
-        setattr(self, f'load_{config_dict["country"]}_{config_dict["default_year"]}', self._get_hourly_load(config_dict["country"], start, end))
-        setattr(self, f'cf_{config_dict["country"]}_{config_dict["default_year"]}', self._get_capacity_factors_renewables(config_dict["country"], start, end))
+        setattr(self, f'load_{config_dict["country"]}_{config_dict["default_year"]}', self._get_or_cache_hourly_load(config_dict["country"], config_dict["default_year"], start, end))
+        setattr(self, f'cf_{config_dict["country"]}_{config_dict["default_year"]}', self._get_or_cache_capacity_factors_renewables(config_dict["country"], config_dict["default_year"], start, end))
 
         # for default year, get load and cf of neighbor countries
         for neighbor in config_dict["neighbor_countries"]:
-            setattr(self, f'load_{neighbor}_{config_dict["default_year"]}', self._get_hourly_load(neighbor, start, end))
-            setattr(self, f'cf_{neighbor}_{config_dict["default_year"]}', self._get_capacity_factors_renewables(neighbor, start, end))
+            setattr(self, f'load_{neighbor}_{config_dict["default_year"]}', self._get_or_cache_hourly_load(neighbor, config_dict["default_year"], start, end))
+            setattr(self, f'cf_{neighbor}_{config_dict["default_year"]}', self._get_or_cache_capacity_factors_renewables(neighbor, config_dict["default_year"], start, end))
 
-        # for scenario years, get cf of main country
+        # for scenario years, get load and cf of main country
         if isinstance(config_dict["scenario_years"], list):
             for scenario_year in config_dict["scenario_years"]:
-                start_scenario = pd.Timestamp(f'{scenario_year}0101', tz="Europe/Brussels")
-                end_scenario = pd.Timestamp(f'{scenario_year+1}0101', tz="Europe/Brussels")
-                setattr(self, f'load_{config_dict["country"]}_{scenario_year}', self._get_hourly_load(config_dict["country"], start_scenario, end_scenario))
+                start_scenario = pd.Timestamp(f'{scenario_year}-01-01', tz="Europe/Brussels")
+                end_scenario = pd.Timestamp(f'{scenario_year + 1}-01-01', tz="Europe/Brussels")
+                setattr(self, f'load_{config_dict["country"]}_{scenario_year}', self._get_or_cache_hourly_load(config_dict["country"], scenario_year, start_scenario, end_scenario))
+                setattr(self, f'cf_{config_dict["country"]}_{scenario_year}', self._get_or_cache_capacity_factors_renewables(config_dict["country"], scenario_year, start_scenario, end_scenario))
         elif isinstance(config_dict["scenario_years"], int):
-                start_scenario = pd.Timestamp(f'{config_dict["scenario_years"]}0101', tz="Europe/Brussels")
-                end_scenario = pd.Timestamp(f'{config_dict["scenario_years"]+1}0101', tz="Europe/Brussels")
-                setattr(self, f'load_{config_dict["country"]}_{config_dict["scenario_years"]}', self._get_hourly_load(config_dict["country"], start_scenario, end_scenario))
-        
+                start_scenario = pd.Timestamp(f'{config_dict["scenario_years"]}-01-01', tz="Europe/Brussels")
+                end_scenario = pd.Timestamp(f'{config_dict["scenario_years"] + 1}-01-01', tz="Europe/Brussels")
+                setattr(self, f'load_{config_dict["country"]}_{config_dict["scenario_years"]}', self._get_or_cache_hourly_load(config_dict["country"], config_dict["scenario_years"], start_scenario, end_scenario))
+                setattr(self, f'cf_{config_dict["country"]}_{config_dict["scenario_years"]}', self._get_or_cache_capacity_factors_renewables(config_dict["country"], config_dict["scenario_years"], start_scenario, end_scenario))
+
         self.technology_costs_all = pd.read_csv('technology-data/outputs/costs_2025.csv', index_col=[0, 1])
         self.technology_costs = {}
         for technology in config_dict["technologies"]:
@@ -42,6 +49,17 @@ class InputHandler():
 
     def _get_hourly_load(self, country:str, start: pd.Timestamp, end:pd.Timestamp) -> pd.DataFrame: 
         return self.client.query_load(country, start=start, end=end)
+    
+    def _get_or_cache_hourly_load(self, country:str, year:int, start: pd.Timestamp, end:pd.Timestamp ) -> pd.DataFrame:
+        path = self.cache_dir / f"load_{country}_{year}.csv"
+        if path.exists():
+            print("Loading loads from CSV:", path)
+            return pd.read_csv(path, index_col=0,  parse_dates=True)
+        
+        print("Querying loads ENTSOE:", country, year)
+        df = self._get_hourly_load(country, start, end)
+        df.to_csv(path)
+        return df
 
 
     def _get_capacity_factors_renewables(self, country:str, start: pd.Timestamp, end:pd.Timestamp) -> pd.DataFrame:
@@ -56,6 +74,17 @@ class InputHandler():
             'wind_onshore_cf':  self._get_cf(country, generation, capacity, 'Wind Onshore'),
             'wind_offshore_cf': self._get_cf(country, generation, capacity, 'Wind Offshore'),
         })
+
+    def _get_or_cache_capacity_factors_renewables(self, country:str, year:int, start: pd.Timestamp, end:pd.Timestamp ) -> pd.DataFrame:
+        path = self.cache_dir / f"capacity_factors_{country}_{year}.csv"
+        if path.exists():
+            print("Loading capacity factors from CSV:", path)
+            return pd.read_csv(path, index_col=0,  parse_dates=True)
+        
+        print("Querying capacity factors from ENTSOE:", country, year)
+        df = self._get_capacity_factors_renewables(country, start, end)
+        df.to_csv(path)
+        return df
     
     @staticmethod
     def _get_cost(df, technology, param):
