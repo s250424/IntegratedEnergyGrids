c818e4c4f2dc11e7410ab803597230c27259d520
Florian Schwill <s250424@dtu.dk>
2026-03-25 13:01:41 +0100

    cached more time-series

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ CONFIG_DICT = {
     "country": "BE",    # must be the same naming convention as used by ENTSO-E
     "neighbor_countries": ["FR", "NL", "DE_LU"],    # must be the same naming convention as used by ENTSO-E
     "default_year": 2023,   # data in good quality available from ENTSO-E for 2015-2024
-    "scenario_years": 2021, # data in good quality available from ENTSO-E for 2015-2024
+    "scenario_years": [2020, 2021, 2022, 2024], # data in good quality available from ENTSO-E for 2015-2024
     "technologies": ["solar", "onwind", "offwind","CCGT","nuclear"]  # must be the same naming convention as in cost list
 }
 
